977652e4640ae9de59de21068a4c10873de1de36
Reiko Makimoto <gracesystems2016@gmail.com>
2020-05-09 00:04:13 +0900

    vue.config.js追加

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // template option使用
+  runtimeCompiler: true,
+  css: {
+    sourceMap: true,
+  },
+  // webpack設定
+  configureWebpack: {
+    devtool: 'source-map',
+  },
+};
