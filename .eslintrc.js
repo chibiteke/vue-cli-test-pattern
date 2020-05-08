27381eda6c29a39f6cd6462948ca85dca7b3b1d2
Reiko Makimoto <gracesystems2016@gmail.com>
2020-05-08 13:58:03 +0900

    eslitとprettierの設定追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,32 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true, // ブラウザ環境有効 （DOM API (documentやonload等)）
+    node: true, // Node.js環境を有効（Node.js 固有の変数や構文 (requireや特殊なトップレベル スコープ等)）
+    es6: true, // ECMAScript 2015 (ES6) で書かれたコードを静的検証する
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: [
+    'plugin:vue/essential', // eslint-plugin-vue
+    'eslint:recommended', // eslintの推奨ルール
+    '@vue/prettier',
+  ],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint',
+    sourceType: 'module', // mapファイル作成
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
       env: {
-        jest: true
-      }
-    }
-  ]
-};
+        jest: true,
+      },
+    },
+  ],
+}
