4a777897f2ffcdb7aaf8ce8164115cfcd38a7c3a
Reiko Makimoto <gracesystems2016@gmail.com>
2020-05-09 01:21:09 +0900

    loginページ追加

---
+++ src/components/templates/LoginView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="login-view">
+    <h1>Login Form</h1>
+    <LoginForm :onlogin="handleLogin" />
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/molecules/BaseLoginForm.vue';
+
+export default {
+  name: 'LoginView',
+
+  components: {
+    LoginForm,
+  },
+
+  methods: {
+    handleLogin() {
+      return true;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.login-view {
+  width: 320px;
+  margin: auto;
+}
+</style>
